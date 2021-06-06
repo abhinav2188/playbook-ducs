e76e1e4037be4a73c1adaac0fd0397556af95517
TheGeekMonk <vasusehgal.mca19.du@gmail.com>
2021-06-07 01:21:39 +0530

    card ui update

--- src/Pages/explore.js
+++ src/Pages/explore.js
@@ -8,7 +8,7 @@ import cardImg3 from "../assets/productivity.jpg";
 
 const Card = (props) => {
   return (
-    <div className="explore-card" style={{ backgroundColor: props.bgColor }}>
+    <div className="explore-card" style={{ background: props.grad }}>
       <div className="card-img">
         <img src={props.imgSrc} alt="" />
       </div>
@@ -35,19 +35,25 @@ const Page = () => {
               key="e1"
               title="Study Material"
               imgSrc={cardImg1}
-              bgColor="#EEBED3"
+              grad="linear-gradient(
+45deg
+, #412FAF, #50C1E8)"
             />
             <Card
               key="e2"
               title="Recreation"
               imgSrc={cardImg2}
-              bgColor="#DEE8F9"
+              grad="linear-gradient(
+45deg
+, #2FB8FF, #95E8DC)"
             />
             <Card
               key="e3"
               title="Productivity"
               imgSrc={cardImg3}
-              bgColor="#C8F2FE"
+              grad="linear-gradient(
+45deg
+, #2587A6, #6FF7E8)"
             />
           </div>
         </div>
