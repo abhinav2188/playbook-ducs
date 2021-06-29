99b2ad1e4dbc13e19fee87e70df682f4a96afe88
Harsh Kumar <harsh.mca19.du@gmail.com>
2021-06-29 23:18:52 +0530

    connects review form to database

--- src/Pages/Components/ReviewForm.jsx
+++ src/Pages/Components/ReviewForm.jsx
@@ -1,13 +1,18 @@
-import React,{useState} from 'react';
+import React,{useState, useContext} from 'react';
 import styles from './ReviewForm.module.scss';
+import {firestoreDB} from "../../services/firebase";
+import { UserContext } from '../../providers/UserProvider';
+import { useHistory } from "react-router-dom";
 
 const ReviewForm = () => {
+
+    const user = useContext(UserContext);
+    const history = useHistory();
+
     const [formData,setFormData] = useState({
         name:"",
         comment:"",
         linkedinId:"",
-        isApproved:false,
-        tags:[]
     });
 
     const updateInput = (e) =>{
@@ -17,6 +22,39 @@ const ReviewForm = () => {
         }));
     }
 
+    const handleSubmit = e => {
+        e.preventDefault()
+        if(!user){
+            alert("Please login to submit a review");
+            history.push("/home");
+        }else{
+            sendData();
+        }
+        
+        setFormData({
+            name:"",
+            comment:"",
+            linkedinId:"",
+        });
+        
+        history.push("/home");
+    }
+    const sendData = () => {
+       
+            firestoreDB.collection("reviews").add({
+            name: formData.name,
+            comment: formData.comment,
+            linkedinId: formData.linkedinId,
+            tags: ["#changethistag"],
+            approved: false
+            }).then(() =>{
+                alert("Form submitted successfully");
+            }).catch((err)=>{
+                alert("Error!");
+                console.error(err);
+            })
+        }
+
     return (
         <form className={styles.reviewForm} onSubmit={(e) => {
             e.preventDefault();
@@ -24,17 +62,17 @@ const ReviewForm = () => {
         }}>
             <label for="reviewer-name" className="field">
                 <span>Name</span>
-                <input id="reviewer-name" type="text" name="name" value={formData.name} onChange={updateInput}/> 
+                <input id="reviewer-name" type="text" name="name" placeholder="Enter your name" value={formData.name || ''} onChange={updateInput} required/> 
             </label>
             <label for="reviewer-linkedin-id">
                 <span>Linkedin Profile Link</span>
-                <input id="reviewer-linkedin-id" type="text" name="linkedinId" value={formData.linkedinId} onChange={updateInput}/> 
+                <input id="reviewer-linkedin-id" type="text" name="linkedinId" placeholder="Enter your linkedIn Id" value={formData.linkedinId || ''} onChange={updateInput} required/> 
             </label>
             <label for="reviewer-comment" className="field">
                 <span>Review</span>
-                <textarea id="reviewer-comment" name="comment" rows={4} value={formData.comment} onChange={updateInput}/> 
+                <textarea id="reviewer-comment" name="comment" rows={4} placeholder="Enter your review" value={formData.comment || ''} onChange={updateInput} required/> 
             </label>
-            <button type="submit" className={styles.submitButton} >
+            <button type="submit" className={styles.submitButton}  onClick={handleSubmit}>
                 Add Review
             </button>
         </form>
